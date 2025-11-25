709adbd867ecb34e9d1118074896736a9c35d8a9
Kev0nthebus <khernandez7993@gmail.com>
2025-11-25 18:10:45 -0500

    Add Day 2 SQL and Python practice files (HAVING, GROUP BY, functions)

---
+++ week1/day02/functions.py
@@ -0,0 +1,9 @@
+# Function practice creating functions asked for as practice problems
+def double(n):
+    return n * 2
+
+def bmi(weight, height):
+    return weight / (height **2)
+
+def is_long(name):
+    return len(name) > 5
